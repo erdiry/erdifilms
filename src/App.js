7633fa4bc643ea3fc59db969f8d0a0743d5100c7
erdimasyudi <72uw6w@gmail.com>
2023-04-25 11:00:48 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import NavigationBar from "./components/NavigationBar";
+import Intro from "./components/Intro";
+import Trending from "./components/Trending";
+import SuperHero from "./components/SuperHero";
+import "./style/landingPage.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <div>
+         {/* intro section */}
+         <div className="myBG">
+            <NavigationBar />
+            <Intro />
+         </div>
+         {/* end of intro */}
+         {/* trending section */}
+         <div className="trending">
+         <Trending/>
+         </div>
+         {/* end trending */}
+         {/* superhero */}
+         <div className="superhero">
+            <SuperHero/>
+         </div>
+         {/* superhero end */}
+      </div>
+   );
 }
 
 export default App;
